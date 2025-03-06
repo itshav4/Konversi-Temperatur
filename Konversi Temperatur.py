55c62cacfedeb16ffa1d7ea172239a1ec6236ecf
itshav4 <fahmi.mulkishava@gmail.com>
2025-03-06 07:48:36 +0700

    Add files via upload

---
+++ Konversi Temperatur.py
@@ -0,0 +1,6 @@
+
+celsius = float(input("Masukkan suhu dalam derajat Celsius: "))
+
+fahrenheit = (celsius * 9/5) + 32
+
+print(f"{celsius:.1f} derajat Celsius = {fahrenheit:.1f} derajat Fahrenheit")
